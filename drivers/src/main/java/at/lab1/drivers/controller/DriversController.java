9e3d2dd48c227dca963f5a98424bca3d189c886b
rel <cristian.ionel@eu.rsystems.com>
2023-10-16 18:27:53 +0300

    implement concurrent tasks limit

--- drivers/src/main/java/at/lab1/drivers/controller/DriversController.java
+++ drivers/src/main/java/at/lab1/drivers/controller/DriversController.java
@@ -3,6 +3,8 @@ package at.lab1.drivers.controller;
 import at.lab1.drivers.dto.Availability;
 import at.lab1.drivers.dto.CompleteRide;
 import at.lab1.drivers.service.DriversService;
+import io.github.bucket4j.Bucket;
+import jakarta.annotation.PostConstruct;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
@@ -10,6 +12,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.context.request.async.AsyncRequestTimeoutException;
 
+import java.time.Duration;
 import java.util.concurrent.Callable;
 
 @Slf4j
@@ -19,29 +22,48 @@ import java.util.concurrent.Callable;
 public class DriversController {
 
     private final DriversService driversService;
+    private Bucket bucket;
+
+    // bucket with capacity 20 tokens and with refilling speed 20 tokens per each minute
+    @PostConstruct
+    public void setupBucket() {
+        this.bucket = Bucket.builder()
+                .addLimit(limit -> limit.capacity(20).refillGreedy(20, Duration.ofMinutes(1)))
+                .build();
+    }
 
     @PutMapping(path = "/{id}/availability")
-    public Callable<ResponseEntity<Availability>> changeAvailability(@PathVariable Long id, @RequestBody Availability availability) {
-        return () -> {
-            try {
-                return ResponseEntity.ok(driversService.changeAvailability(id, availability));
-            } catch (AsyncRequestTimeoutException ex) {
-                log.error("Request timeout on change_driver_availability with driver_id : {}", id);
-                return ResponseEntity.status(HttpStatus.REQUEST_TIMEOUT).build();
-            }
-        };
+    public Callable<ResponseEntity<?>> changeAvailability(@PathVariable Long id, @RequestBody Availability availability) {
+        if (bucket.tryConsume(1)) {
+            return () -> {
+                try {
+                    return ResponseEntity.ok(driversService.changeAvailability(id, availability));
+                } catch (AsyncRequestTimeoutException ex) {
+                    log.error("Request timeout on change_driver_availability with driver_id : {}", id);
+                    return ResponseEntity.status(HttpStatus.REQUEST_TIMEOUT).build();
+                }
+            };
+        } else {
+            return () -> ResponseEntity.status(HttpStatus.TOO_MANY_REQUESTS)
+                    .body("Too Many Requests - Please try again later.");
+        }
     }
 
     @PutMapping(path = "/{id}/ride")
-    public Callable<ResponseEntity<CompleteRide>> completeRide(@PathVariable Long id, @RequestBody CompleteRide state) {
-        return () -> {
-            try {
-                return ResponseEntity.ok(driversService.completeRide(id, state));
-            } catch (AsyncRequestTimeoutException ex) {
-                log.error("Request timeout on complete_ride with driver_id : {}", id);
-                return ResponseEntity.status(HttpStatus.REQUEST_TIMEOUT).build();
-            }
-        };
+    public Callable<ResponseEntity<?>> completeRide(@PathVariable Long id, @RequestBody CompleteRide state) {
+        if (bucket.tryConsume(1)) {
+            return () -> {
+                try {
+                    return ResponseEntity.ok(driversService.completeRide(id, state));
+                } catch (AsyncRequestTimeoutException ex) {
+                    log.error("Request timeout on complete_ride with driver_id : {}", id);
+                    return ResponseEntity.status(HttpStatus.REQUEST_TIMEOUT).build();
+                }
+            };
+        } else {
+            return () -> ResponseEntity.status(HttpStatus.TOO_MANY_REQUESTS)
+                    .body("Too Many Requests - Please try again later.");
+        }
     }
 
     @GetMapping(path = "/status")
--- rides/src/main/java/at/lab1/rides/controller/RidesController.java
+++ rides/src/main/java/at/lab1/rides/controller/RidesController.java
@@ -2,57 +2,84 @@ package at.lab1.rides.controller;
 
 import at.lab1.rides.dto.*;
 import at.lab1.rides.service.RidesService;
-import lombok.AllArgsConstructor;
+import io.github.bucket4j.Bucket;
+import jakarta.annotation.PostConstruct;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.context.request.async.AsyncRequestTimeoutException;
 
+import java.time.Duration;
 import java.util.concurrent.Callable;
 
 @Slf4j
 @RestController
-@AllArgsConstructor
+@RequiredArgsConstructor
 @RequestMapping("api/rides")
 public class RidesController {
 
     private final RidesService ridesService;
+    private Bucket bucket;
+
+    // bucket with capacity 20 tokens and with refilling speed 20 tokens per each minute
+    @PostConstruct
+    public void setupBucket() {
+        this.bucket = Bucket.builder()
+                .addLimit(limit -> limit.capacity(20).refillGreedy(20, Duration.ofMinutes(1)))
+                .build();
+    }
 
     @GetMapping(path = "/{id}")
-    public Callable<ResponseEntity<Ride>> getRide(@PathVariable Long id) {
-        return () -> {
-            try {
-                return ResponseEntity.ok(ridesService.getRide(id));
-            } catch (AsyncRequestTimeoutException ex) {
-                log.error("Request timeout on ID : {}", id);
-                return ResponseEntity.status(HttpStatus.REQUEST_TIMEOUT).build();
-            }
-        };
+    public Callable<ResponseEntity<?>> getRide(@PathVariable Long id) {
+        if (bucket.tryConsume(1)) {
+            return () -> {
+                try {
+                    return ResponseEntity.ok(ridesService.getRide(id));
+                } catch (AsyncRequestTimeoutException ex) {
+                    log.error("Request timeout on ID : {}", id);
+                    return ResponseEntity.status(HttpStatus.REQUEST_TIMEOUT).build();
+                }
+            };
+        } else {
+            return () -> ResponseEntity.status(HttpStatus.TOO_MANY_REQUESTS)
+                    .body("Too Many Requests - Please try again later.");
+        }
     }
 
     @PostMapping
-    public Callable<ResponseEntity<RequestRideResponse>> requestRide(@RequestBody RequestRide ride) {
-        return () -> {
-            try {
-                return ResponseEntity.ok(ridesService.requestRide(ride));
-            } catch (AsyncRequestTimeoutException ex) {
-                log.error("Request timeout on ride_request : {}", ride);
-                return ResponseEntity.status(HttpStatus.REQUEST_TIMEOUT).build();
-            }
-        };
+    public Callable<ResponseEntity<?>> requestRide(@RequestBody RequestRide ride) {
+        if (bucket.tryConsume(1)) {
+            return () -> {
+                try {
+                    return ResponseEntity.ok(ridesService.requestRide(ride));
+                } catch (AsyncRequestTimeoutException ex) {
+                    log.error("Request timeout on ride_request : {}", ride);
+                    return ResponseEntity.status(HttpStatus.REQUEST_TIMEOUT).build();
+                }
+            };
+        } else {
+            return () -> ResponseEntity.status(HttpStatus.TOO_MANY_REQUESTS)
+                    .body("Too Many Requests - Please try again later.");
+        }
     }
 
     @PutMapping(path = "/{id}/state")
-    public Callable<ResponseEntity<ChangeRideState>> changeRideState(@PathVariable Long id, @RequestBody ChangeRideState state) {
-        return () -> {
-            try {
-                return ResponseEntity.ok(ridesService.changeRideState(id, state));
-            } catch (AsyncRequestTimeoutException ex) {
-                log.error("Request timeout on change_ride_state with id : {}", id);
-                return ResponseEntity.status(HttpStatus.REQUEST_TIMEOUT).build();
-            }
-        };
+    public Callable<ResponseEntity<?>> changeRideState(@PathVariable Long id, @RequestBody ChangeRideState state) {
+        if (bucket.tryConsume(1)) {
+            return () -> {
+                try {
+                    return ResponseEntity.ok(ridesService.changeRideState(id, state));
+                } catch (AsyncRequestTimeoutException ex) {
+                    log.error("Request timeout on change_ride_state with id : {}", id);
+                    return ResponseEntity.status(HttpStatus.REQUEST_TIMEOUT).build();
+                }
+            };
+        } else {
+            return () -> ResponseEntity.status(HttpStatus.TOO_MANY_REQUESTS)
+                    .body("Too Many Requests - Please try again later.");
+        }
     }
 
     @GetMapping(path = "/status")
